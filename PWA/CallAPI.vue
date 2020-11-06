df5bc349e8dc91f62368fdbe93e0b450958731da
boowecall <72959616+boowecall@users.noreply.github.com>
2020-11-06 20:55:14 +0700

    Create CallAPI.vue

---
+++ PWA/CallAPI.vue
@@ -0,0 +1,19 @@
+methods: {
+    callApi() {
+      fetch("//boowecall.github.io/gl/PWA//api/ping", {
+        // NEW - add a Content-Type header
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+        .then(async response => {
+          if (response.ok) {
+            this.apiError = false;
+            this.result = await response.json();
+          } else {
+            this.apiError = true;
+          }
+        })
+        .catch(() => (this.apiError = true));
+    }
+  }
